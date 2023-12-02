2c362c7c6730a19c4680a09334eb1381c6e35f10
Rabia Kuran <rabiakuran34@gmail.com>
2023-12-02 16:01:31 +0300

    updatee

--- OpenWeatherMapCard/Program.cs
+++ OpenWeatherMapCard/Program.cs
@@ -12,6 +12,7 @@ namespace OpenWeatherMapCard
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //update
         }
     }
 }
